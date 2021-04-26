e08f7d93603689d1885232369ce7a93720e064f5
albertoDM13 <jesus_duk_12@hotmail.com>
2021-04-25 19:23:23 -0500

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import React from "react";
+/*import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
 import ButtonAdd from "./button-add";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+*/
